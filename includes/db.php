b6cbb67cfabde9bd907ba4a03b99f6f703c6135c
andrei-L1 <adr.lonsania@gmail.com>
2026-07-02 10:33:50 +0800

    feat: add database connection configuration for local and remote environments in db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,32 @@
 <?php
 $host = 'localhost';
-$db   = 'teacher_leave_system';
+$db = 'teacher_leave_system';
 $user = 'root';
 $pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // show errors
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // clean arrays
+    PDO::ATTR_EMULATE_PREPARES => false, // real prepared statements
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
+
+
+
+
+$host = 'sql301.byethost18.com';
+$db = 'b18_42316990_leave_system';
+$user = 'b18_42316990';
+$pass = 'sdlms@1234';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
